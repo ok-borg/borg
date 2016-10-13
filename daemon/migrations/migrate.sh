70f58fc02532d21966f02114252330dd41bf3e30
jeremyletang <letang.jeremy@gmail.com>
2016-10-13 20:41:15 +0100

    add mysql migration

---
+++ daemon/migrations/migrate.sh
@@ -0,0 +1,7 @@
+
+HOST="192.168.99.100"
+PORT="3307"
+
+mysql -v --host=$HOST -P $PORT -u root --password=root < 0_create_db.sql
+mysql -v --host=$HOST -P $PORT -u root --password=root < 1_create_users.sql
+mysql -v --host=$HOST -P $PORT -u root --password=root < 2_create_organizations.sql
