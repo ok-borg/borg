d64a2e583415bdfabf33623ec2f2211238e2e51a
jeremyletang <letang.jeremy@gmail.com>
2016-10-13 23:21:04 +0100

    add migration for organization_join_links

--- daemon/migrations/migrate.sh
+++ daemon/migrations/migrate.sh
@@ -5,3 +5,4 @@ PORT="3307"
 mysql -v --host=$HOST -P $PORT -u root --password=root < 0_create_db.sql
 mysql -v --host=$HOST -P $PORT -u root --password=root < 1_create_users.sql
 mysql -v --host=$HOST -P $PORT -u root --password=root < 2_create_organizations.sql
+mysql -v --host=$HOST -P $PORT -u root --password=root < 3_create_organizations_join_links.sql
